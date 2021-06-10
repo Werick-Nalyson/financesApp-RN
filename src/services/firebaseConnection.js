5ebe2496bcdf4341c542981fb9b354227506913a
Werick Nalyson <wericknalyson@gmail.com>
2021-06-10 09:25:06 -0300

    add final app

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  // ...
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
